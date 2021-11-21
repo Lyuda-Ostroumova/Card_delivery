43404a6be04a7b4ddb5097e85a18f90a4a144e9d
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-11-21 22:01:38 +0300

    Initial commit

---
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -0,0 +1,32 @@
+package ru.netology.delivery;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.Configuration;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldRegisterCardDelivery() {
+
+
+        open("http://localhost:9999");
+        $("[data-test-id='city'] [class='input__control']").sendKeys("Вологда"); // город
+        $("[data-test-id='date'] [class='input__control']").sendKeys("24.11.2021");// дата
+        $("[data-test-id='name'] [class='input__control']").sendKeys("Пупкин Василий"); // ФИ
+        $("[data-test-id='phone'] [class='input__control']").sendKeys("+79632587411"); // тел
+        $("[data-test-id='agreement'] [class='checkbox__box']").click(); // согласие
+        $("[class='button__content']").click(); // отправка формы
+        $("[data-test-id='notification'] [class='notification__title']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+
+    }
+}
