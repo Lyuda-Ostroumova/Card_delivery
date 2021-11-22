ba435af543bc998d930593618f1ce273ab2cdba6
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-11-22 20:02:05 +0300

    Added Dselenide.headless=true

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -1,14 +1,12 @@
 package ru.netology.delivery;
 
-
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-
-
 import java.time.Duration;
 
+import com.codeborne.selenide.Condition;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 
 public class CardDeliveryTest {
 
