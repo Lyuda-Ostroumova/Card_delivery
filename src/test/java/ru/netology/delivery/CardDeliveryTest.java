fc99fbddf683bb34adda228084d2683c7e7eaa1f
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-11-26 22:21:54 +0300

    Changed data format

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -2,11 +2,9 @@ package ru.netology.delivery;
 
 import org.junit.jupiter.api.Test;
 
-
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 
 import com.codeborne.selenide.Condition;
 
@@ -25,18 +23,20 @@ public class CardDeliveryTest {
 
         LocalDate date = LocalDate.now();
         date = date.plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd" + "." + "MM" + "." + "yyyy");
+        String formattedString = date.format(formatter);
 
 
         open("http://localhost:9999");
         $("[data-test-id='city'] [class='input__control']").sendKeys("Вологда"); // город
-        $("[data-test-id='date'] [class='input__control']").doubleClick().setValue(String.valueOf(date));// дата
+        $("[data-test-id='date'] [class='input__control']").doubleClick().setValue(String.valueOf(formattedString));// дата
         $("[data-test-id='name'] [class='input__control']").sendKeys("Пупкин Василий"); // ФИ
         $("[data-test-id='phone'] [class='input__control']").sendKeys("+79632587411"); // тел
         $("[data-test-id='agreement'] [class='checkbox__box']").click(); // согласие
         $("[class='button__content']").click(); // отправка формы
         $("[data-test-id='notification'] [class='notification__title']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15));
-//        $("[class='notification__content']").shouldHave(Condition.text(String.valueOf(date)));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на" + " " + String.valueOf(formattedString)));
 
     }
 
